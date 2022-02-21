4a66960df088600555fd659e828cd77f541f4b99
cjlaborde <cjlaborde@gmail.com>
2022-02-21 17:49:59 -0400

    The Wrapper Object-vm to access vue instance|setData to change the data from component in the test

--- README.md
+++ README.md
@@ -30,3 +30,32 @@
 ``` 
 
 4. Use mount as default approach as it helps to detect potential errors regarding child componets early
+
+
+### The Wrapper Object
+1. vm is the access to the vue intance that you can interact with
+2. setData
+3. Vue Test Utils removed synchronous updating from the library: https://github.com/vuejs/vue-test-utils/issues/1137
+4. Hence why Engame was not appearing so you need to add async and await
+```js
+test("ListComponent", async () => {
+  const wrapper = mount(List);
+  //   console.log(wrapper);
+  // we can pick data using vm which access the vue instance
+  const movies = wrapper.vm.marvelMovies;
+  // user setData to update the actual dataset
+
+  /* we are passing the key of the data set that we want to overrule */
+  // To not lose the current existing movies, we just spread them into the same array, again
+  await wrapper.setData({ marvelMovies: [...movies, "Endgame"] });
+
+  console.log(wrapper.html());
+  expect(wrapper.html()).toMatchSnapshot();
+});
+```
+
+
+5. jest-serializer-vue is the package that was added that makes snapshot easier to read
+6. Because of jest.config.js `  snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],`
+7. We can remove .html()
+8. `  expect(wrapper).toMatchSnapshot();`
--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,21 +1,35 @@
-import TesComponent from "@/test";
+import TestComponent from "@/test";
 import List from "@/list";
 
 import { mount, shallowMount } from "@vue/test-utils";
 
 // Snapshot test //
 test("mount a vue component", () => {
-  const wrapper = mount(TesComponent, {
+  const wrapper = mount(TestComponent, {
     propsData: {
       value: "VueSchool",
     },
   });
-  expect(wrapper.html()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
   //   console.log(wrapper);
 });
 
 // shallowMount
-test("ListComponent Shallow", () => {
-  console.log(mount(List).html());
-  console.log(shallowMount(List).html());
+// test.skip("ListComponent Shallow", () => {
+//   console.log(mount(List).html());
+//   console.log(shallowMount(List).html());
+// });
+
+test("ListComponent", async () => {
+  const wrapper = mount(List);
+  //   console.log(wrapper);
+  // we can pick data using vm which access the vue instance
+  const movies = wrapper.vm.marvelMovies;
+  // user setData to update the actual dataset
+
+  /* we are passing the key of the data set that we want to overrule */
+  // To not lose the current existing movies, we just spread them into the same array, again
+  await wrapper.setData({ marvelMovies: [...movies, "Endgame"] });
+
+  expect(wrapper).toMatchSnapshot();
 });
