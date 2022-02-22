b74b77fdf281b08f6747ad327c7f292fdba6ee42
cjlaborde <cjlaborde@gmail.com>
2022-02-22 19:58:43 -0400

    Exercises

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,15 @@
-import UserList from '@/exercise-1';
-import { mount } from '@vue/test-utils';
+import UserList from "@/exercise-1";
+import { mount } from "@vue/test-utils";
+import faker from "@faker-js/faker";
+
+test("component renders the users", () => {
+  const wrapper = mount(UserList, {
+    propsData: {
+      users: [faker.name.findName()],
+    },
+  });
+
+  const li = wrapper.find("li");
+  //   console.log(wrapper.props("users")[0]);
+  expect(li.text()).toBe(wrapper.props("users")[0]);
+});
